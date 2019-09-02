9949e554e32cb95b81a2beac2ed65174064e10ab
Jay Kim <kim.jay33@gmail.com>
2019-09-02 10:47:42 -0400

    added logic to catch if tweet too long or if tweet is empty.

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var cred = require('./config.js');
 const fs = require('fs');
 var T = new twitter(cred);
 
-// function to find all key values on matching date
-function getKeyByValue(object, value){
-  return Object.keys(object).filter(key => object[key] === value);
-}
+// reading/parsing json file scraped from python script
+var dfile = fs.readFileSync('./webScraper/release.json');
+let data = JSON.parse(dfile);
 
-// converting date to MMM DD format ex: Jan 1
+// function to convert date to MMM DD format ex: Jan 1
 Date.prototype.toShortFormat = function(){
   var months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   var day = this.getDate();
@@ -17,15 +16,16 @@ Date.prototype.toShortFormat = function(){
 
   return months[months_index] + " " + day;
 }
-// setting formatted date to vars
+// setting formatted date to var
 var date = new Date();
 var formatted = date.toShortFormat();
 
-// reading/parsing json file scraped from python script
-var dfile = fs.readFileSync('./webScraper/release.json');
-let data = JSON.parse(dfile);
+// function to find all key values on matching date
+function getKeyByValue(object, value){
+  return Object.keys(object).filter(key => object[key] === value);
+}
 
-// set 'keys' variable to list of keys with matching date to find title
+// set 'keys' variable to array of keys with matching date with converted date
 var keys = getKeyByValue(data.date, formatted);
 
 // for statement to find all titles with date keys
@@ -33,11 +33,18 @@ var tweet = new Array();
 for (index in keys){
   tweet += data.title[keys[index]] + '\n';
 }
-
-T.post('statuses/update', {status: tweet}, function(error, tweet, response){
-  if (error){
-    console.log(error)
-  }
-  console.log(tweet);
-  console.log(response);
-})
+if (tweet.length > 280){
+  //need logic to calculate size of tweet to comply with twitter
+  console.log(tweet.length + ' is too long and will not post.')
+} else if (tweet.length == 0){
+  console.log('There are no releases today')
+} else{
+  //post twitter status
+  T.post('statuses/update', {status: tweet}, function(error, tweet, response){
+    if (error){
+      console.log(error)
+    }
+    console.log(tweet);
+    console.log(response);
+  })
+}
